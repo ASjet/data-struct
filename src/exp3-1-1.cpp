3ed00450621f9e1adbf8e5c13e7c51104a0f602f
ASjet <Aryan.ITFS@iCloud.com>
2021-04-24 12:36:31 +0800

    feat(src/exp3-1*):finish exp3

--- src/exp3-1-1.cpp
+++ src/exp3-1-1.cpp
@@ -11,12 +11,31 @@ const array_size_t MAX_SIZE = 50;
 ////////////////////////////////////////////////////////////////////////////////
 int main(void)
 {
-    Stack * S = new Stack(MAX_SIZE);
-    for(int i = 1; i != 7; ++i)
+    elem_t e;
+    cout << "顺序栈S的基本运算如下:" << endl;
+    cout << "    (1)初始化栈S" << endl;
+    Stack *S = new Stack(MAX_SIZE);
+    cout << "    (2)栈为" << ((S->empty()) ? "空" : "非空") << endl;
+    cout << "    (3)依次进栈元素";
+    for (int i = 1; i < 7; ++i)
+    {
+        cout << ((i == 1)? '\0': ',') << i;
         S->push(i);
+    }
+    cout << endl;
+    S->GetTop(&e);
+    cout << "    GetTop()=" << e << endl;
 
-    cout << S->GetTop() << endl;
-    cout << S->length() << endl;
+    cout << "    (4)栈为" << ((S->empty()) ? "空" : "非空") << endl;
+    cout << "    (5)出栈序列:";
+    while(!S->empty())
+    {
+        S->pop(&e);
+        cout << e << ((S->empty())?'\0':' ');
+    }
+    cout << endl;
+    cout << "    (6)栈为" << ((S->empty()) ? "空" : "非空") << endl;
+    cout << "    (7)释放栈" << endl;
     delete S;
     return 0;
 }
